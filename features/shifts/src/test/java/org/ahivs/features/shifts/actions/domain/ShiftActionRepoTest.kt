b32b1cbda262668879b7b9b7e8c3ab6fd50f7732
laaptu <sanimap@gmail.com>
2021-02-14 12:28:18 +1100

    Group shift start and end tests

--- features/shifts/src/test/java/org/ahivs/features/shifts/actions/domain/ShiftActionRepoTest.kt
+++ features/shifts/src/test/java/org/ahivs/features/shifts/actions/domain/ShiftActionRepoTest.kt
@@ -1,6 +1,8 @@
 package org.ahivs.features.shifts.actions.domain
 
 import org.ahivs.features.shifts.actions.domain.ShiftActionResponse.Companion.SHIFT_IN_PROGRESS
+import org.ahivs.features.shifts.actions.domain.ShiftActionResponse.Companion.SHIFT_NOT_STARTED
+import org.ahivs.features.shifts.actions.domain.ShiftActionResponse.Companion.SUCCESS_SHIFT_END
 import org.ahivs.features.shifts.actions.domain.ShiftActionResponse.Companion.SUCCESS_SHIFT_START
 import org.ahivs.features.shifts.data.ShiftApiService
 import org.ahivs.features.shifts.data.ShiftData
@@ -9,12 +11,14 @@ import org.ahivs.shared.testing.BaseTest
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class ShiftActionRepoTest : BaseTest() {
+
     @Mock
     lateinit var shiftApiService: ShiftApiService
 
@@ -32,58 +36,119 @@ class ShiftActionRepoTest : BaseTest() {
         shiftActionRepo = ShiftActionRepo(shiftApiService, logger)
     }
 
-    @DisplayName(
-        """
+    @Nested
+    inner class ShiftStartTests() {
+        @DisplayName(
+            """
         GIVEN valid response,
         WHEN startShift is called,
         THEN Success must be returned
-    """
-    )
-    @Test
-    fun validResponseForStartShiftReturnsSuccess() {
-        runBlockingTest {
-            `when`(shiftApiService.startShift(shiftData)).thenReturn(SUCCESS_SHIFT_START)
-            val response = shiftActionRepo.startShift(shiftData)
-            assertTrue(response is Success)
-        }
-    }
-
-    @DisplayName(
         """
+        )
+        @Test
+        fun validResponseForStartShiftReturnsSuccess() {
+            runBlockingTest {
+                `when`(shiftApiService.startShift(shiftData)).thenReturn(SUCCESS_SHIFT_START)
+                val response = shiftActionRepo.startShift(shiftData)
+                assertTrue(response is Success)
+            }
+        }
+
+        @DisplayName(
+            """
         GIVEN invalid response,
         WHEN startShift is called,
         THEN Failure must be returned with response message
-    """
-    )
-    @Test
-    fun invalidResponseForStartShiftReturnsFailure() {
-        val invalidResponse = SHIFT_IN_PROGRESS
-        runBlockingTest {
-            `when`(shiftApiService.startShift(shiftData)).thenReturn(invalidResponse)
-            val response = shiftActionRepo.startShift(shiftData)
-            assertTrue(response is Failure)
-            assertTrue((response as Failure).errorMsg == invalidResponse)
-        }
-    }
-
-    @DisplayName(
         """
+        )
+        @Test
+        fun invalidResponseForStartShiftReturnsFailure() {
+            val invalidResponse = SHIFT_IN_PROGRESS
+            runBlockingTest {
+                `when`(shiftApiService.startShift(shiftData)).thenReturn(invalidResponse)
+                val response = shiftActionRepo.startShift(shiftData)
+                assertTrue(response is Failure)
+                assertTrue((response as Failure).errorMsg == invalidResponse)
+            }
+        }
+
+        @DisplayName(
+            """
         GIVEN Exception ,
         WHEN startShift is called,
         THEN Failure must be returned with exception message
-    """
-    )
-    @Test
-    fun exceptionForStartShiftReturnsFailure() {
-        val exceptionMsg = "Network failure"
-        val exception = Exception(exceptionMsg)
-        runBlockingTest {
-            `when`(shiftApiService.startShift(shiftData)).thenAnswer {
-                throw exception
+        """
+        )
+        @Test
+        fun exceptionForStartShiftReturnsFailure() {
+            val exceptionMsg = "Network failure"
+            val exception = Exception(exceptionMsg)
+            runBlockingTest {
+                `when`(shiftApiService.startShift(shiftData)).thenAnswer {
+                    throw exception
+                }
+                val response = shiftActionRepo.startShift(shiftData)
+                assertTrue(response is Failure)
+                assertTrue((response as Failure).errorMsg == exceptionMsg)
             }
-            val response = shiftActionRepo.startShift(shiftData)
-            assertTrue(response is Failure)
-            assertTrue((response as Failure).errorMsg == exceptionMsg)
+        }
+    }
+
+    @Nested
+    inner class ShiftEndTest {
+        @DisplayName(
+            """
+        GIVEN valid response,
+        WHEN endShift is called,
+        THEN Success must be returned
+        """
+        )
+        @Test
+        fun validResponseForEndShiftReturnsSuccess() {
+            runBlockingTest {
+                `when`(shiftApiService.endShift(shiftData)).thenReturn(SUCCESS_SHIFT_END)
+                val response = shiftActionRepo.endShift(shiftData)
+                assertTrue(response is Success)
+            }
+        }
+
+        @DisplayName(
+            """
+        GIVEN invalid response,
+        WHEN endShift is called,
+        THEN Failure must be returned with response message
+        """
+        )
+        @Test
+        fun invalidResponseForEndShiftReturnsFailure() {
+            val invalidResponse = SHIFT_NOT_STARTED
+            runBlockingTest {
+                `when`(shiftApiService.endShift(shiftData)).thenReturn(invalidResponse)
+                val response = shiftActionRepo.endShift(shiftData)
+                assertTrue(response is Failure)
+                assertTrue((response as Failure).errorMsg == invalidResponse)
+            }
+        }
+
+        @DisplayName(
+            """
+        GIVEN Exception ,
+        WHEN endShift is called,
+        THEN Failure must be returned with exception message
+        """
+        )
+        @Test
+        fun exceptionForEndShiftReturnsFailure() {
+            val exceptionMsg = "Network failure"
+            val exception = Exception(exceptionMsg)
+            runBlockingTest {
+                `when`(shiftApiService.endShift(shiftData)).thenAnswer {
+                    throw exception
+                }
+                val response = shiftActionRepo.endShift(shiftData)
+                assertTrue(response is Failure)
+                assertTrue((response as Failure).errorMsg == exceptionMsg)
+            }
         }
     }
 }
