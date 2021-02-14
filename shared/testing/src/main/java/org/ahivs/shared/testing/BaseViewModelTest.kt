f3cc84cfa32f98996e809f3b3c9724a6a461844f
laaptu <sanimap@gmail.com>
2021-02-14 11:15:12 +1100

    Importing common testing helper classes for coroutines, livedata

---
+++ shared/testing/src/main/java/org/ahivs/shared/testing/BaseViewModelTest.kt
@@ -0,0 +1,19 @@
+package org.ahivs.shared.testing
+
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.RegisterExtension
+
+
+@ExtendWith(TaskExecutorRule::class)
+abstract class BaseViewModelTest {
+    @RegisterExtension
+    @JvmField
+    val coroutinesTestRule = CoroutinesTestRule()
+
+    @OptIn()
+    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) {
+        coroutinesTestRule.testDispatcher.runBlockingTest(block)
+    }
+}
